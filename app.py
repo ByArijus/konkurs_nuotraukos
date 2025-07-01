07c9e29edd155e0d83ae1e902285ae5deb908503
ByArijus <byarijusphotography@outlook.com>
2025-07-01 13:43:00 +0300

    app.py

--- app.py
+++ app.py
@@ -179,6 +179,7 @@ def delete_participant(code):
 
     flash('Dalyvis pašalintas kartu su vartotoju ir nuotraukomis', 'success')
     return redirect(url_for('index'))
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
